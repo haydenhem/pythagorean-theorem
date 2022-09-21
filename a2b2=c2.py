5185d763ac1e5b51fa1c18c65d5be37bc7d7f70e
hayden <99268163+haydenhem@users.noreply.github.com>
2022-09-21 09:41:19 -0400

    graphing

--- a2b2=c2.py
+++ a2b2=c2.py
@@ -1,4 +1,6 @@
 import math
+import numpy as np 
+import matplotlib.pyplot as plt
 
 
 print("a^2 + b^2 = c^2 calculator")
@@ -18,19 +20,20 @@ if any(ask.lower() == f for f in ["a", "1"]):
         print("That isnt a number! Retype it please!")
         c = input("Type In c: ")
     c = int(c)
-    a = math.sqrt(c**2 - b**2) 
-    print("A = " + str(a))
+    a = math.sqrt(c**2 - b**2)
+    print("A = " + str(a)) 
 if any(ask.lower() == f for f in ["b", "2"]):
-    b = input("Type In A: ")
-    while not b.isdigit():
+    a = input("Type In A: ")
+    while not a.isdigit():
         print("That isnt a number! Retype it please!")
-        b = input("Type In A: ")
+        a = input("Type In A: ")
     c = input("Type In c: ")
     while not c.isdigit():
         print("That isnt a number! Retype it please!")
         c = input("Type In c: ")
-    b = math.sqrt(int(c) ** 2 - int(a) ** 2)
-    print("B = " + b)
+    c = int(c)
+    b = math.sqrt(int(c)**2 - int(a)**2)
+    print("B = " + str(b))
 if any(ask.lower() == f for f in ["c", "3"]):
     a = input("Type In A: ")
     while not a.isdigit():
@@ -44,5 +47,24 @@ if any(ask.lower() == f for f in ["c", "3"]):
     if math.sqrt(c).is_integer():
      c = int(math.sqrt(c))
      print("C^2 = "+str(c))
+     c = int(c)
     else:
      print("C = " + "√" + str(c))
+     c = math.sqrt(c)
+     print("(Decimal) c = " + str(c))
+     c = "√" + str(int(a) ** 2 + int(b)**2)
+        
+a = int(a)
+b = int(b)
+
+X = np.array([[0+int(a),0], [0+a,0+b], [0, 0]])
+Y = ['red', 'red', 'red']
+
+plt.figure()
+plt.scatter(X[:, 0], X[:, 1], s = 170, color = Y[:])
+plt.grid()
+t1 = plt.Polygon(X[:3,:], color=Y[0])
+plt.gca().add_patch(t1)
+plt.title("Triangle Grapher")
+plt.text(a / 2, b / 2 + b / 8, 'Hypotinus = '+ str(c), fontsize = 10, bbox = dict(facecolor = 'red', alpha = 0.5), rotation=35)
+plt.show()
